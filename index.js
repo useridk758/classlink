37856ef0284fbd69a11d1acca50e455481105c61
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-04-01 16:58:44 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`GradeLink server is running on http://localhost:${PORT}`);
+});
