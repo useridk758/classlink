c5ffa7301ee6148a3a3653b1f22191277600e2ad
useridk758 <9z8r9zwdsh@privaterelay.appleid.com>
2026-04-01 17:06:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the current directory
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
@@ -11,5 +10,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`GradeLink server is running on http://localhost:${PORT}`);
+    console.log(`GradeLink Hub live on http://localhost:${PORT}`);
 });
